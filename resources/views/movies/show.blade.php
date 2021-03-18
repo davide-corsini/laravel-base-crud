b2aa25aff67ddcf7ba8680135eb5f2a912bd535b
davide <davidecorsinimilano@gmail.com>
2021-03-18 16:17:11 +0100

    working on layout

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,10 +3,12 @@
 @section('title','PAGINA SHOW')
 
 @section('content')
-    <h1>WELCOME TO HOME SHOW page make now</h1>
-
-    <p>Trama {{$movie->trama}}</p> 
-    <p>E' costato circa: {{$movie->cost}}mln</p>   
-    <p>Uscito il: {{$movie->Data_uscita}}</p>
+    <h1 style="text-align: center;">Dettagli Film</h1>
+    <div class="box-movie">
+        <p>Trama {{$movie->trama}}</p> 
+        <p>E' costato circa: {{$movie->cost}}mln</p>   
+        <p>Uscito il: {{$movie->Data_uscita}}</p>
+        <img style="width: 200px; height: 200px; border-radius: 50%;" src="{{$movie->poster}}" alt="">
+    </div>
 
 @endsection
